8aff4e303c2784e79eb8a604b78533bf6aebe614
dnewintern1 <radhe3127@gmail.com>
2023-11-28 22:30:15 +0530

    ui screen

---
+++ app/src/main/java/com/base/internshipproject/Workoutfile.java
@@ -0,0 +1,14 @@
+package com.base.internshipproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Workoutfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_workout);
+    }
+}
